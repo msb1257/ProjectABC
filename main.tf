07c8b119bd5f99b36e678249981a7ba13c0f92dc
msb1257 <makkenasaradhibabudevops@gmail.com>
2026-01-17 08:26:28 +0530

    Initialize Terraform configuration for AWS S3 bucket

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-demo-tf-bucketsana-12345"
+}
